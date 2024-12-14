91205c8c551cecca3977a320dfeeadb14649103f
michleesc <michlee.septian.c@gmail.com>
2024-12-14 13:00:21 +0700

    update

--- contact_process.php
+++ contact_process.php
@@ -1,37 +1,43 @@
 <?php
+// Pastikan form menggunakan metode POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Alamat email tujuan
+    $to = "michlee.septian.c@gmail.com";
 
-    $to = "rockybd1995@gmail.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+    // Mengambil data dari form
+    $name = htmlspecialchars($_POST['name']);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
 
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    // Validasi sederhana
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email format.";
+        exit;
+    }
 
-    $subject = "You have a message from your Bitmap Photography.";
+    // Header email
+    $headers = "From: " . $email . "\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $logo = 'img/logo.png';
-    $link = '#';
+    // Isi email
+    $body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Contact Email</title></head><body>";
+    $body .= "<h2>New Message from Website</h2>";
+    $body .= "<p><strong>Name:</strong> {$name}</p>";
+    $body .= "<p><strong>Email:</strong> {$email}</p>";
+    $body .= "<p><strong>Subject:</strong> {$subject}</p>";
+    $body .= "<p><strong>Message:</strong></p>";
+    $body .= "<p>{$message}</p>";
+    $body .= "</body></html>";
 
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
-
-    $send = mail($to, $subject, $body, $headers);
-
-?>
+    // Mengirim email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Email sent successfully. Thank you for contacting us!";
+    } else {
+        echo "Failed to send email. Please try again later.";
+    }
+} else {
+    echo "Access denied.";
+}
